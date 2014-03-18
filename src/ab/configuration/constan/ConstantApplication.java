60bc0ca60813671f9c71f2b9b023e5ff781df6c1
Angga BS <anggabs86@gmail.com>
2014-03-18 12:10:22 +0700

    Update ConstantApplication

--- src/ab/configuration/constan/ConstantApplication.java
+++ src/ab/configuration/constan/ConstantApplication.java
@@ -4,11 +4,11 @@ public class ConstantApplication
 {
 
     public static final String C_APP_NAME = "Muntilan Application Server";
-    public static final String C_AB_DEVELOPMENT = "coba-coba";
+    public static final String C_AB_DEVELOPMENT = "DEVELOPMENT";
     public static final String C_VERSION_APPLICATION = "0.1";
-    public static final String C_ID_APPLICATION = "L41L1-M46F1R0H";
+    public static final String C_ID_APPLICATION = "Blue-Eyes";
     public static final String C_AUTHOR = "Angga B.S";
-    public static final String C_EMAIL_AUTHOR = "anggabs@rocketmail.com";
+    public static final String C_EMAIL_AUTHOR = "anggabs86@gmail.com";
     public static final boolean DEBUG_MODE = true;
     public static String defaultPHPPathSetting = "src/konfigurasi/php.properties";
     public static final String C_COMMAND_COMPILE_PHP_CODE = "-q";
